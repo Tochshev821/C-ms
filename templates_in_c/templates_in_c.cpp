d3bc3e69aa1ba378835f51daa39630826a7725cb
Георгий Тощев <gtoshev2@mail.ru>
2022-09-16 22:19:15 +0300

    Сделал 1 задание и частично 3 е. В 3 многое не учел. Доделать 2е задание

---
+++ templates_in_c/templates_in_c.cpp
@@ -0,0 +1,61 @@
+﻿
+#include <iostream>
+
+template <typename T>
+T max(T a, T b)
+{
+    if (b < a) // если 1 больше 2го возвращает 1ый
+    {
+        std::cout << "a" << std::endl;
+        return a;
+    }
+    else // иначе возвращает б 
+    {
+        std::cout << "b" << std::endl;
+        return b;
+    }
+    //return(b < a ? a : b);
+}
+template <typename T>
+auto min(T a, T b)
+{
+    if (b > a) // если 1 больше 2го возвращает 1ый
+    {
+        std::cout << "a" << std::endl;
+        return a;
+    }
+    else // иначе возвращает б 
+    {
+        std::cout << "b" << std::endl;
+        return b;
+    }
+    //return(b < a ? a : b);
+}
+
+
+template <typename T>
+void swap(T& a, T& b)
+{
+    T t = a;
+    a = b;
+    b = t;
+}
+
+
+int main()
+{
+    //std::cout << "Hello World!\n";
+    int a = 12;
+    int b = 22;
+    double c = 2.2;
+    double f = 5.5;
+    std::cout << "max is " << max(a, b) << std::endl;
+
+    //std::cout << max(f, c) << std::endl;
+    std::cout <<"min is " << min(a, b) << std::endl;
+
+    swap(a, b);
+    std::cout << "a = " << a << "\t b = " << b << std::endl;
+
+}
+
---
+++ test/test.cpp
@@ -0,0 +1,39 @@
+﻿
+#include <iostream>
+/// решим квадратное уравнение
+unsigned sqrt_int(long L)// вычисление квадратного корня из int переменной
+{
+	unsigned div = 1, rslt = 0;
+	while (L > 0)
+	{
+		L -= div, div += 2;
+		rslt += L < 0 ? 0 : 1;
+	}
+	return rslt;
+}
+
+int f(int& a, int& b, int& c, int& root1, int& root2) { // вычисление дискриминанта, поиск корней
+    double d = b*b - 4 * a * c;
+    if (d > 0) {
+        root1 = ((-1 * b) + sqrt_int(d)) / (2 * a);
+        root2 = ((-1 * b) - sqrt_int(d)) / (2 * a);
+        return 2;
+    }
+    if (d == 0) {
+        root1 = -1 * (b / (2 * a));
+        return 1;
+    }
+    return 0;
+}
+
+int main() {
+    int a, b, c;
+    a = 2; //первый коэффициент
+    b = 5; //второй коэффициент
+    c = -3; //свободный член
+
+    int r1, r2; // корни 
+    int root_count = f(a, b, c, r1, r2);
+    std::cout << root_count;
+    return 0;
+}
